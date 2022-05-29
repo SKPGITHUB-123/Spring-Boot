52369bd79b9e698200bf94c89f5f9c331c73ff78
unknown <sangram.pradhan@iserveu.in>
2022-05-29 21:22:04 +0530

    Initial Commit to setup

---
+++ src/main/java/com/iserveu/dmt/UpdateCustomerLimitByList1Application.java
@@ -0,0 +1,13 @@
+package com.iserveu.dmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateCustomerLimitByList1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateCustomerLimitByList1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iserveu/dmt/UpdateCustomerLimitByList1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iserveu.dmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateCustomerLimitByList1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
